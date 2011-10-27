2ea057e98828831776c9e7fc3e084f55e372e65a
andrewgroot <QuantumChaos7@gmail.com>
2011-10-27 17:12:35 -0400

    Wrote variable parts of the symbol_table and added stuff for functions.

--- h/symbol_table.h
+++ h/symbol_table.h
@@ -9,7 +9,7 @@
 
 class symbol_table {
   private:
-    std::vector<std::map<std::string, s_expression> > vars;
+    std::vector<std::map<std::string, s_expression*> > vars;
 
   public:
     // Add another level to the stack.
@@ -17,9 +17,11 @@ class symbol_table {
     // Remove the top of the stack.
     void pop();
     // Returns the value that the given variable name is bound to. 
-    s_expression get(std::string);
+    s_expression* get(std::string);
     // Create a binding of a variable name to a value in the top-most scope of the stack. 
-    void set(std::string, s_expression);
+    void set(std::string, s_expression*);
+    // Define a function with a name, parameters, and body
+    void define(std::string, s_expression*, s_expression*);
 };
 
 #endif
--- src/symbol_table.cpp
+++ src/symbol_table.cpp
@@ -1,19 +1,34 @@
 #include "../h/symbol_table.h"
 using namespace std;
-// std::vector<std::map<std::string, var> > vars;
+// std::vector<std::map<std::string, s_expression*> > vars;
 
 void symbol_table::push() {
-
+    map<string, s_expression> m;
+    vars.push_back(m);
 }
 
 void symbol_table::pop() {
-
+    vars.pop_back();
 }
 
-var symbol_table::get(string name) {
-
+s_expression* symbol_table::get(string name) {
+    for (int i=vars.size-1; i>=0; i--) {
+        if (vars[i].count(name)) {
+            return (vars[i])[name];
+        }
+    }
+    return NULL;
 }
 
-void symbol_table::set(string name, string value) {
-
+void symbol_table::set(string name, s_expression* value) {
+    (vars.back())[name] = value;
 }
+
+void symbol_table::define(string name, s_expression* params, s_expression* body) {
+    
+}
+
+s_expression* symbol_table::call(string name) {
+    
+}
+
