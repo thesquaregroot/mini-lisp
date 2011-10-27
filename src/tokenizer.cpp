9bf946560521bee54f64717763d08be700ccdd7b
andrewgroot <QuantumChaos7@gmail.com>
2011-10-27 12:18:49 -0400

    Fixed up the tokenizer some (possibly completely).

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -2,7 +2,6 @@
 #include "../h/tokenizer.h"
 using namespace std;
 // istream ins;
-// string buffer;
 // char look_ahead;
 
 
@@ -12,7 +11,6 @@ tokenizer::tokenizer(istream* in) {
 
 token tokenizer::get() {
     token t;
-    buffer.clear();
     // skip whitespace
     while (!ins.eof() && isspace(lookahead = ins.get()));
     
@@ -22,31 +20,61 @@ token tokenizer::get() {
     switch(lookahead) {
         case '(':
             t.type = L_PAREN;
+            t.lex_val = '(';
             break;
         case ')':
             t.type = R_PAREN;
+            t.lex_val = ')';
             break;
         case '.':
             t.type = DOT;
+            t.lex_val = '.';
             break;
         case '-':
         case '+':
             // save +/-
-            buffer += lookahead;
+            t.lex_val += lookahead;
             // expect a number afterward
+            bool one_or_more = false;
             while (!ins.eof() && isdigit(lookahead = ins.get())) {
-                buffer += lookahead;
+                t.lex_val += lookahead;
+                one_or_more = true;
             }
-            if (isspace(lookahead) || lookahead == ')') {
-                t.type = ATOM;
+            if (ins.eof()) {
+                t.type = F_END;
+            } else {
+                if ((isspace(lookahead) || lookahead == ')') && one_or_more) {
+                    cin.unget();
+                    t.type = ATOM;
+                } else {
+                    t.lex_val += lookahead;
+                    t.type = ERROR;
+                }
             }
             break;
-        default: {
+        default:
             if (isalpha(lookahead)) {
-                
+                t.lex_val += lookahead;
+                while (!ins.eof() && isalnum(lookahead = ins.get())) {
+                    t.lex_val += lookahead;
+                }
+                if (ins.eof()) {
+                    t.type = F_END;
+                } else {
+                    if ((isspace(lookahead) || lookahead == ')') && one_or_more) {
+                        cin.unget();
+                        t.type = ATOM;
+                    } else {
+                        t.lex_val += lookahead;
+                        t.type = ERROR;
+                    }
+                }
+            } else {
+                t.lex_val += lookahead;
+                t.type = ERROR;
             }
             break;
     }
-    return ERROR;
+    return t;
 }
 
