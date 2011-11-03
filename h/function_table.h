29552aac8b82a80eae1a90392790bff152b4206a
andrewgroot <QuantumChaos7@gmail.com>
2011-11-03 08:12:19 -0400

    Aparently made some changes to the function_table header.  Really just backing up my progress before leaving for class.

--- h/function_table.h
+++ h/function_table.h
@@ -3,15 +3,19 @@
 
 #include <vector>
 #include <string>
+#include "./symbol_table.h"
+#include "./s_expression.h"
 
 class function_table {
   private:
+    std::map<std::string, vector<std::string> > params;
+    std::map<std::string, s_expression> bodies;
 
   public:
     // Define a function with a name, parameters, and body
-    void define(std::string, std::vector<std::string>, s_expression*);
-    // Call a function with the given parameters
-    s_expression* call(std::string, s_expression*);
+    void define(std::string, std::vector<std::string>&, s_expression&);
+    // Call a function with the given parameters and enviroment
+    s_expression* call(std::string, s_expression&, symbol_table&);
 }
 
 #endif
