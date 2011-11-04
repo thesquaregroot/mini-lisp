196e6442d36f588e796fac5317fc8164d50f581f
andrewgroot <QuantumChaos7@gmail.com>
2011-11-03 23:24:35 -0400

    Moved function_table back into symbol_table -- it should be there -- and started implementing.  Still need to hammer out the specifics of using it -- mainly function naming stuff but it's moving along nicely and shouldn't be too bad.

--- h/symbol_table.h
+++ h/symbol_table.h
@@ -9,17 +9,27 @@
 
 class symbol_table {
   private:
+    // variables
     std::vector<std::map<std::string, s_expression*> > vars;
+    // functions
+    std::map<std::string, vector<std::string> > params;
+    std::map<std::string, s_expression> bodies;
 
   public:
     // Add another level to the stack.
     void push();
     // Remove the top of the stack.
     void pop();
+
     // Returns the value that the given variable name is bound to. 
     s_expression* get(std::string);
     // Create a binding of a variable name to a value in the top-most scope of the stack. 
     void set(std::string, s_expression);
+
+    // Define a function with a name, parameters, and body
+    void define(std::string, std::vector<std::string>&, s_expression&);
+    // Call a function with the given parameters and enviroment
+    s_expression* call(std::string, s_expression&, symbol_table&);
 };
 
 #endif
--- src/symbol_table.cpp
+++ src/symbol_table.cpp
@@ -1,9 +1,31 @@
 #include "../h/symbol_table.h"
 using namespace std;
-// std::vector<std::map<std::string, s_expression*> > vars;
 
+// std::vector<std::map<std::string, s_expression&> > vars;
+// std::map<std::string, s_expression&> params;
+// std::map<std::string, s_expression&> bodies;
+
+// private
+s_expression* symbol_table::get(string name) {
+    for (int i=vars.size-1; i>=0; i--) {
+        if (vars[i].count(name) > 0) {
+            return (vars[i])[name];
+        }
+    }
+    return NULL;
+}
+
+void symbol_table::set(string name, s_expression& value) {
+    (vars.back())[name] = value;
+}
+
+void symbol_table::exec(s_expression&) {
+    if ()
+}
+
+// public
 void symbol_table::push() {
-    map<string, s_expression> m;
+    map<string, s_expression&> m;
     vars.push_back(m);
 }
 
@@ -11,16 +33,7 @@ void symbol_table::pop() {
     vars.pop_back();
 }
 
-s_expression* symbol_table::get(string name) {
-    for (int i=vars.size-1; i>=0; i--) {
-        if (vars[i].count(name)) {
-            return (vars[i])[name];
-        }
-    }
-    return NULL;
-}
-
-void symbol_table::set(string name, s_expression* value) {
-    (vars.back())[name] = value;
+s_expression& symbol_table::eval(s_expression&) {
+    
 }
 
